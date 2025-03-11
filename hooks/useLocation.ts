5fe8211a344c8e54653c66ca56c6e7ff0880f699
muskan <muskanloach984@gmail.com>
2025-03-11 16:46:28 +0530

    uselocation hook

---
+++ hooks/useLocation.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
+
+const useLocation = () => {
+    const [error, setError] = useState("");
+    const [latitude, setLatitude] = useState<number | null>(null);
+    const [longitude, setLongitude] = useState<number | null>(null);
+
+    const getUserLocation = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+
+        if (status !== "granted") {
+            setError("Permission to access location was not granted");
+            return;
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        if (location?.coords) {
+            const { latitude, longitude } = location.coords;
+            console.log("Latitude and Longitude:", latitude, longitude);
+            setLatitude(latitude);
+            setLongitude(longitude);
+
+            let response = await Location.reverseGeocodeAsync({ latitude, longitude });
+            console.log("User Location:", response);
+        }
+    };
+
+    useEffect(() => {
+        getUserLocation();
+    }, []);
+
+    return { latitude, longitude, error };
+};
+
+export default useLocation;
